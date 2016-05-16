70979d47f367045936b0dbb48d2087480b1f23b9
Ewerton Filipe <xewertonsys@gmail.com>
2016-05-16 16:48:02 -0300

    Tratamento de exceção

---
+++ src/classes/DadoInvalidoException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author Ewerton
+ */
+public class DadoInvalidoException extends Exception {
+    
+    DadoInvalidoException(String msg) {
+        super("Campo em branco ou invlálido");
+    }
+}
