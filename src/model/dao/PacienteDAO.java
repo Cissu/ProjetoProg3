3b6b7f13f12343bb0acdad47c978f3671d3d3e3d
Rhavila Medeiros <rhavila_pmn@hotmail.com>
2016-05-18 00:12:52 -0300

    Adição de metodo
    metodo deletar acrescentado na classe paciente DAO

--- src/model/dao/PacienteDAO.java
+++ src/model/dao/PacienteDAO.java
@@ -23,13 +23,12 @@ public class PacienteDAO {
         PreparedStatement stmt = null;
 
         try {
-            stmt = con.prepareStatement("insert into paciente(id, nome, rg, cpf, dataNasc, endereco) values (?,?,?,?,?,?)");
-            stmt.setInt(1, p.getId());
-            stmt.setString(2, p.getNome());
-            stmt.setString(3, p.getRg());
-            stmt.setString(4, p.getCpf());
-            stmt.setString(5, p.getDataNasc());
-            stmt.setObject(6, p.getEndereco());
+            stmt = con.prepareStatement("insert into paciente(nome, rg, cpf, dataNasc, endereco) values (?,?,?,?,?)");
+            stmt.setString(1, p.getNome());
+            stmt.setString(2, p.getRg());
+            stmt.setString(3, p.getCpf());
+            stmt.setString(4, p.getDataNasc());
+            stmt.setObject(5, p.getEndereco());
 
             stmt.executeUpdate();
             JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
@@ -41,8 +40,8 @@ public class PacienteDAO {
         }
 
     }
-    
-        public void update(Paciente p) {
+
+    public void update(Paciente p) {
         Connection con = Conectar.getConnection();
         PreparedStatement stmt = null;
 
@@ -60,6 +59,25 @@ public class PacienteDAO {
             JOptionPane.showMessageDialog(null, "Atualizado com sucesso!");
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Erro ao atualizar " + e);
+        } finally {
+            Conectar.closeConnection(con, stmt);
+
+        }
+
+    }
+
+    public void delete(Paciente p) {
+        Connection con = Conectar.getConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = con.prepareStatement("delete from paciente where id=?");
+            stmt.setInt(1, p.getId());
+
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Excluido com sucesso!");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao excluir " + e);
         } finally {
             Conectar.closeConnection(con, stmt);
 
