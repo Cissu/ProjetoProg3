2c93c7f507314943ee7f8dec35876086fc18386c
Ewerton Filipe <xewertonsys@gmail.com>
2016-05-25 00:31:23 -0300

    Alterações

--- src/principal/FormPaciente.java
+++ src/principal/FormPaciente.java
@@ -189,18 +189,17 @@ public class FormPaciente extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        principal.CadastroPaciente p = new principal.CadastroPaciente();
-            p.setVisible(true);
-
-    }//GEN-LAST:event_jButton1ActionPerformed
-
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         try {
             readPaciente();
         } catch (DadoInvalidoException ex) {
         }
     }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        principal.CadastroPaciente p = new principal.CadastroPaciente();
+        p.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
