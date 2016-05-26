c0969cbc3d9f8850c89f789379628e230f048e15
Ewerton Filipe <xewertonsys@gmail.com>
2016-05-26 01:13:52 -0300

    Alterações, campo data

--- src/principal/Agenda.java
+++ src/principal/Agenda.java
@@ -44,58 +44,81 @@ public class Agenda extends javax.swing.JFrame {
 
         jLabel1.setText("Data:");
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(20, 110, 33, 24);
+        jLabel1.setBounds(10, 190, 33, 24);
 
-        jLabel3.setText("Paciente");
+        jLabel3.setText("Pesquisar Paciente:");
         jPanel1.add(jLabel3);
-        jLabel3.setBounds(260, 30, 57, 24);
+        jLabel3.setBounds(260, 30, 100, 24);
         jPanel1.add(jTextField1);
         jTextField1.setBounds(360, 30, 250, 30);
 
         jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagens/agenda.png"))); // NOI18N
         jPanel1.add(jLabel6);
-        jLabel6.setBounds(10, 10, 100, 80);
+        jLabel6.setBounds(10, 10, 100, 100);
 
         jButton1.setText("Buscar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         jPanel1.add(jButton1);
-        jButton1.setBounds(20, 270, 70, 30);
+        jButton1.setBounds(70, 230, 70, 30);
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
             new String [] {
-                "Nome", "Procedimento", "Dentista", "Data", "Horário"
+                "Id", "Nome", "Procedimento", "Dentista", "Especialidade", "Data", "Horário"
             }
-        ));
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
         jScrollPane1.setViewportView(jTable1);
         if (jTable1.getColumnModel().getColumnCount() > 0) {
-            jTable1.getColumnModel().getColumn(0).setPreferredWidth(300);
+            jTable1.getColumnModel().getColumn(0).setResizable(false);
+            jTable1.getColumnModel().getColumn(0).setPreferredWidth(30);
+            jTable1.getColumnModel().getColumn(1).setPreferredWidth(300);
+            jTable1.getColumnModel().getColumn(2).setPreferredWidth(300);
+            jTable1.getColumnModel().getColumn(3).setResizable(false);
+            jTable1.getColumnModel().getColumn(3).setPreferredWidth(150);
+            jTable1.getColumnModel().getColumn(4).setPreferredWidth(150);
         }
 
         jPanel1.add(jScrollPane1);
-        jScrollPane1.setBounds(270, 70, 690, 430);
+        jScrollPane1.setBounds(190, 70, 920, 480);
 
         jButton3.setText("Limpar");
         jPanel1.add(jButton3);
-        jButton3.setBounds(130, 270, 70, 30);
+        jButton3.setBounds(70, 470, 70, 30);
         jPanel1.add(jDateChooser1);
-        jDateChooser1.setBounds(60, 110, 120, 30);
+        jDateChooser1.setBounds(50, 190, 120, 30);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 988, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 1110, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 526, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 556, Short.MAX_VALUE)
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+       
+    }//GEN-LAST:event_jButton1ActionPerformed
 
 
    
