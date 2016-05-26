ffdc1839ef9d670f574c732621b0a208462361da
Rhavila Medeiros <rhavila_pmn@hotmail.com>
2016-05-26 07:38:36 -0300

    FluxoCaixa

--- src/model/dao/DespesaGanhoDAO.java
+++ src/model/dao/DespesaGanhoDAO.java
@@ -9,6 +9,10 @@ import bancodedados.Conectar;
 import classes.DespesaGanho;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -16,22 +20,60 @@ import javax.swing.JOptionPane;
  * @author Snowgal
  */
 public class DespesaGanhoDAO {
-    public void inserirGanho(DespesaGanho dp){
+
+    public void inserirGanho(DespesaGanho dp) {
         Connection con = Conectar.getConnection();
         PreparedStatement stmt = null;
-        
+
         try {
             stmt = con.prepareStatement("insert into despesaganho (tipo, descricao, valor, data) values ('Ganho', ?, ?, ?)");
             stmt.setString(1, dp.getDescricao());
             stmt.setDouble(2, dp.getValor());
             stmt.setString(3, dp.getData());
             stmt.executeUpdate();
-            
-            
+
             JOptionPane.showMessageDialog(null, "Inserido com Sucesso!");
-            
+
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Erro ao inserir " + e);
+        } finally {
+            Conectar.closeConnection(con, stmt);
         }
     }
+
+    public void create(DespesaGanho dp) {
+        Connection con = Conectar.getConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = con.prepareStatement("insert into despesaganho (tipo, descricao, valor, data) values (?, ?, ?, ?)");
+            stmt.setString(1, dp.getTipo());
+            stmt.setString(2, dp.getDescricao());
+            stmt.setDouble(3, dp.getValor());
+            stmt.setString(4, dp.getData());
+            stmt.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Inserido com Sucesso!");
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao inserir " + e);
+        } finally {
+            Conectar.closeConnection(con, stmt);
+        }
+    }
+
+//    public void totalDespesa() {
+//        Connection con = Conectar.getConnection();
+//        PreparedStatement stmt = null;
+//        try {
+//
+//            ResultSet rs = stmt.executeQuery("SELECT sum(valor)AS total FROM despesaganho");
+//            while (rs.next()) {
+//                txtTotal.setText(Double.toString(rs.getDouble("total")));
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 }
